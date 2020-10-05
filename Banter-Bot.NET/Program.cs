3d4075d2466343a2b350fad9095d06e1e7b51818
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-10-05 17:02:26 +0200

    Initial commit

---
+++ Banter-Bot.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banter_Bot.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
