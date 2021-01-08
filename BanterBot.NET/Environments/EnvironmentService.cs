fe1c538f4c14f38c7043ed4604b8ad9bf731a8a9
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-01-08 15:13:47 +0100

    Add warning when replacing existing environment variable

--- BanterBot.NET/Environments/EnvironmentService.cs
+++ BanterBot.NET/Environments/EnvironmentService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using BanterBot.NET.Dependencies;
+using BanterBot.NET.Logging;
 
 namespace BanterBot.NET.Environments
 {
@@ -21,6 +22,11 @@ namespace BanterBot.NET.Environments
                     var split = line.Split('=', 2);
                     var key = split[0];
                     var value = split[1];
+
+                    if (Environment.GetEnvironmentVariable(key) != null)
+                    {
+                        Logger.WarningS($"Replacing existing environment variable {key}");
+                    }
 
                     Environment.SetEnvironmentVariable(key, value);
                 }
